9edb7300f06e71ff15402a95136edc6bd3f0d0b8
wutongtoby <wutongtoby@gmail.com>
2019-11-06 00:54:49 +0800

    let R->3 and there are still bugs probably in the neighbor verification or in BFS

--- project.cpp
+++ project.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
 #include <fstream>
 #include <queue>
+#include <stdio.h>
 
+#define up 0
+#define down 1
+#define left 2
+#define right 3
 using namespace std;
 
 class graph;
 
-enum {up = 0, down = 1, left = 2, right = 3};
 enum {white, grey, black};
 
 class node {
@@ -31,25 +35,28 @@ public:
     graph(void); // constructor
     void clean(void); // to print the path of clean
     void set_BFS(void);
+    void print_BFS(void);
 };
 
 
 graph:: graph(void) 
 {
-    fstream fin;
+    //fstream fin;
 
-    fin.open("floor.data", ios::in);
-    fin >> col >> row >> battery;
+    //fin.open("floor.data", ios::in);
+    cin >> col >> row >> battery;
 
     num_node = col * row;
     array = new node[num_node];
     predecessor = new int[num_node];
     distance = new int[num_node];
 
+
     for (int i = 0; i < num_node; i++) {
-        fin >> array[i].type;
-        if (array[i].type == 'R')
+        cin >> array[i].type;
+        if (array[i].type == 3) {
             R_position = i;
+        }
         array[i].position = i;
         array[i].Is_clean = 0;
         array[i].neighbor[up] = -1;
@@ -57,11 +64,10 @@ graph:: graph(void)
         array[i].neighbor[left] = -1;
         array[i].neighbor[right] = -1;
     }
-    fin.close();
-
+    //fin.close();
     // set the neighbor data
     for (int i = 0; i < num_node; i++) {
-        if (array[i].typea != 1) {
+        if (array[i].type != 1) {
             if (i / col == 0) // R is at the top of the graph
                 array[i].neighbor[down] = i + row;
             else if (i / col == row - 1) // R is at the bottom of the graph
@@ -73,7 +79,7 @@ graph:: graph(void)
             else {
                 if (array[i - col].type != 1) 
                     array[i].neighbor[up] = i - row;
-                if (node_aray[i + col].type != 1)
+                if (array[i + col].type != 1)
                     array[i].neighbor[down] = i + row;
                 if (array[i - 1].type != 1)
                     array[i].neighbor[left] = i - 1;
@@ -82,6 +88,7 @@ graph:: graph(void)
             }
         }
     }
+  
     set_BFS(); // run the BSF on the graph
 }
 
@@ -97,37 +104,48 @@ void graph::set_BFS(void)
         predecessor[i] = -1;
         distance[i] = col + row - 2;
     }
-
+    
     queue<int> q;
     int i = R_position;
-
     for (int j = 0; j < num_node; j++) {
-        if (colour[j] == white) {
-            colour[j] = grey;
-            distance[j] = 0;
-            predecessor[j] = -1;
+        if (colour[i] == white) {
+            colour[i] = grey;
+            distance[i] = 0;
+            predecessor[i] = -1;
             q.push(i);
-        }
-        while (!q.empty()) {
-            int u = q.front();
-            for (int k = 0; k < 4; k++) {
-                int current_pos = array[u].neighbor[k];
-                if (colour[current_pos] == white) {
-                    colour[current_pos] = grey;
-                    distance[current_pos] = distance[u] + 1;
-                    predecessor[current_pos] = u;
-                    q.push(current_pos);
+            while (!q.empty()) {
+                int u = q.front();
+                for (int k = 0; k < 4; k++) {
+                    int current_pos = array[u].neighbor[k];
+                    if (colour[current_pos] == white) {
+                        colour[current_pos] = grey;
+                        distance[current_pos] = distance[u] + 1;
+                        predecessor[current_pos] = u;
+                        q.push(current_pos);
+                    }
                 }
+                q.pop();
+                colour[u] = black;
             }
-            q.pop();
-            colour[u] = black;
         }
         i = j;
     }
     delete [] colour;
 }
+
+void graph:: print_BFS(void) 
+{
+    for (int i = 0; i < num_node; i++) {
+        if (array[i].type != 1) {
+            cout << i << "th vertex has distance "<<distance[i];
+            cout << ", and it's predecessor is " << predecessor[i] << endl;
+        }
+    }
+}
 int main(void)
 {
     graph mygraph;
+    mygraph.print_BFS();
+
     return 0;
 }
