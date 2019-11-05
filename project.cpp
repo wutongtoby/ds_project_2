3ee32b1a06cf3481b5ad4ff582a6c88b0f7e6fc0
wutongtoby <wutongtoby@gmail.com>
2019-11-05 23:53:15 +0800

    implement the BFS

--- project.cpp
+++ project.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 #include <fstream>
+#include <queue>
 
 using namespace std;
 
 class graph;
 
 enum {up = 0, down = 1, left = 2, right = 3};
+enum {white, grey, black};
 
 class node {
 friend graph;
 private:
     int type; // 1 or 0 or R
     int position; // the position of node
-    bool neighbor[4]; // from 0 to 3 are up down left right
+    int neighbor[4]; // from 0 to 3 are up down left right
     bool Is_clean; // 1 is clean, 0 is dirty
 };
 
@@ -50,10 +52,10 @@ graph:: graph(void)
             R_position = i;
         array[i].position = i;
         array[i].Is_clean = 0;
-        array[i].neighbor[up] = 0;
-        array[i].neighbor[down] = 0;
-        array[i].neighbor[left] = 0;
-        array[i].neighbor[right] = 0;
+        array[i].neighbor[up] = -1;
+        array[i].neighbor[down] = -1;
+        array[i].neighbor[left] = -1;
+        array[i].neighbor[right] = -1;
     }
     fin.close();
 
@@ -61,22 +63,22 @@ graph:: graph(void)
     for (int i = 0; i < num_node; i++) {
         if (array[i].typea != 1) {
             if (i / col == 0) // R is at the top of the graph
-                array[i].neighbor[down] = 1;
+                array[i].neighbor[down] = i + row;
             else if (i / col == row - 1) // R is at the bottom of the graph
-                array[i].neighbor[up] = 1;
+                array[i].neighbor[up] = i - row;
             else if (i % col == 0) // R is at the left wall
-                array[i].neighbor[right] = 1;
+                array[i].neighbor[right] = i - 1;
             else if (i % col == col - 1) // R is at the right wall
-                array[i].neighbor[left] = 1;
+                array[i].neighbor[left] = i + 1;
             else {
                 if (array[i - col].type != 1) 
-                    array[i].neighbor[up] = 1;
+                    array[i].neighbor[up] = i - row;
                 if (node_aray[i + col].type != 1)
-                    array[i].neighbor[down] = 1;
+                    array[i].neighbor[down] = i + row;
                 if (array[i - 1].type != 1)
-                    array[i].neighbor[left] = 1;
+                    array[i].neighbor[left] = i - 1;
                 if (array[i + 1].type != 1)
-                    array[i].neighbor[right] = 1;
+                    array[i].neighbor[right] = i + 1;
             }
         }
     }
@@ -85,7 +87,44 @@ graph:: graph(void)
 
 void graph::set_BFS(void)
 {
+    int *colour = new int[num_node];
+    
+    for (int i = 0; i < num_node; i++) {
+        if (array[i].type != 1)
+            colour[i] = white;
+        else
+            colour[i] = black;
+        predecessor[i] = -1;
+        distance[i] = col + row - 2;
+    }
 
+    queue<int> q;
+    int i = R_position;
+
+    for (int j = 0; j < num_node; j++) {
+        if (colour[j] == white) {
+            colour[j] = grey;
+            distance[j] = 0;
+            predecessor[j] = -1;
+            q.push(i);
+        }
+        while (!q.empty()) {
+            int u = q.front();
+            for (int k = 0; k < 4; k++) {
+                int current_pos = array[u].neighbor[k];
+                if (colour[current_pos] == white) {
+                    colour[current_pos] = grey;
+                    distance[current_pos] = distance[u] + 1;
+                    predecessor[current_pos] = u;
+                    q.push(current_pos);
+                }
+            }
+            q.pop();
+            colour[u] = black;
+        }
+        i = j;
+    }
+    delete [] colour;
 }
 int main(void)
 {
